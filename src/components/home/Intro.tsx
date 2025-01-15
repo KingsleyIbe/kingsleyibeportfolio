d5f068bfa097fc069131371702e025aae1bdf2a7
kingsleyibe <ibekingsley66@gmail.com>
2025-01-15 17:19:06 +0100

    Add css animation

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router';
 
 const Intro = () => {
-  return (
-    <div className="px-[200px] mt-[50px] mb-[150px]">
-      <h1 className="text-5xl md:text-8xl sm:text-7xl xl:text-9xl font-bold opacity-[0.8]">Kingsley Ibe</h1>
-      <p className="text-[50px] opacity-[0.6]" >I build things for the web</p>
-      <Link to="/about-me" className="text-[35px] line hover:opacity-[0.6]">Read more about me → </Link>
-    </div>
-  )
+    const spark = (event: MouseEvent | TouchEvent) => {
+        const sparkElement = document.createElement("i");
+        const x = event instanceof MouseEvent ? event.pageX : event.touches[0].pageX;
+        const y = event instanceof MouseEvent ? event.pageY : event.touches[0].pageY;
+    
+        sparkElement.style.left = `${x}px`;
+        sparkElement.style.top = `${y}px`;
+        sparkElement.style.scale = `${Math.random() * 2 + 1}`;
+        sparkElement.style.setProperty("--x", getTransitionValue());
+        sparkElement.style.setProperty("--y", getTransitionValue());
+    
+        document.body.appendChild(sparkElement);
+    
+        setTimeout(() => {
+          document.body.removeChild(sparkElement);
+        }, 2000);
+      };
+    
+      const getTransitionValue = (): string => {
+        return `${Math.random() * 400 - 200}px`;
+      };
+    
+      useEffect(() => {
+        const handleMouseMove = (event: MouseEvent) => {
+          spark(event);
+        };
+    
+        const handleTouchStart = (event: TouchEvent) => {
+          spark(event);
+        };
+    
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("touchstart", handleTouchStart);
+    
+        return () => {
+          document.removeEventListener("mousemove", handleMouseMove);
+          document.removeEventListener("touchstart", handleTouchStart);
+        };
+      }, []);
+    
+    return (
+        <div className="px-[100px] mt-[50px] mb-[150px]">
+            <h1 className="text-5xl md:text-8xl sm:text-7xl xl:text-9xl font-bold opacity-[0.8]">Kingsley Ibe</h1>
+            <p className="text-[50px] opacity-[0.6]" >I build things for the web</p>
+            <Link to="/about-me" className="text-[35px] line hover:opacity-[0.6]">Read more about me → </Link>
+        </div>
+    )
 };
 
 export default Intro;
